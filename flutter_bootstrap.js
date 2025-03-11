aa7a4cf26d5ce3edcdb2cd81cef6a6b14ea79756
Tanguy Falconnet <tanguy.falconnet@pewty.fr>
2025-03-11 08:47:10 +0100

    upgrade

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1317865401"
+    serviceWorkerVersion: "2823783929"
   }
 });
